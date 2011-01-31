c6f689041acc82750dea8940b4233f25877ba927
Ryan Newton <ryan.newton@alum.mit.edu>
2011-01-31 09:46:03 -0500

    Almost victory!  I tried duncan's strategy of rewriting the .cabal file. Alas this means that I need to be careful not to check-in the rewritten version :(. Also, the current approach only works if cabal configure and cabal install are run separately.

--- Setup.hs
+++ Setup.hs
@@ -8,32 +8,66 @@ import Distribution.PackageDescription
 import Distribution.Simple.LocalBuildInfo	
 import System.Cmd(system) 
 import System.Exit
+import System.IO.Unsafe
 import System.Directory
 import Data.Maybe
 import Data.List
+import Data.IORef
 import Debug.Trace
 
-
 main = do
-  putStrLn$ "*** Running Setup.hs..."
+  putStrLn$ "  [intel-aes] Running Setup.hs..."
 
   defaultMainWithHooks $
    simpleUserHooks 
-    { postConf = my_postConf
-    , preBuild = my_preBuild
+    { postConf  = my_postConf
+    , confHook  = my_conf
+    , preBuild  = my_preBuild
     , buildHook = my_build
+    , instHook  = my_install
     }
 
+-- global_var_hack = unsafePerformIO (newIORef "UNSETVARIABLE")
+-- Even worse, a file will be our global variable:
+tmpfile = ".temp.install_dir.txt"
+
+
 --------------------------------------------------------------------------------
--- We override postConf to keep it from complaining about a missing library:
+my_conf :: (GenericPackageDescription, HookedBuildInfo) -> ConfigFlags -> IO LocalBuildInfo
+my_conf (gpd,hbi) flags = do 
+  (confHook simpleUserHooks) (gpd,hbi) flags
+
+--------------------------------------------------------------------------------
+-- we override postConf to keep it from complaining about a missing library:
 my_postConf :: Args -> ConfigFlags -> PackageDescription -> LocalBuildInfo -> IO ()
 my_postConf args conf desc localinfo = do
 --  Just desc <- readDesc simpleUserHooks
   let buildinfos = allBuildInfo desc
 
-  putStrLn$ "\n EXTRA libraries: "++ show (map extraLibs buildinfos)
+  putStrLn$ "\n  [intel-aes] Extra libraries initially: "++ show (map extraLibs buildinfos)
   let desc2 = stripDesc desc
-  putStrLn$ "\n LIBS POST STRIP "++ show (map extraLibs $ allBuildInfo desc2)
+  putStrLn$ "  [intel-aes] Stripped extra libraries to: "++ show (map extraLibs $ allBuildInfo desc2)
+
+  let libd  = libdir$ absoluteInstallDirs desc localinfo NoCopyDest
+  -- This is lame but I'm not sure how to get the same information at the preBuild step.
+  -- writeIORef global_var_hack libd
+  putStrLn$ "  [intel-aes] Determined install dir to be: " ++ show libd 
+  writeFile tmpfile libd
+  putStrLn$ "  [intel-aes] Recorded install dir in " ++ show tmpfile
+
+  orig <- readFile "intel-aes.cabal"
+  let bak = "intel-aes.cabal.bak."
+      findBak n = do
+        e <- doesFileExist (bak ++ show n)
+	if e then findBak (n+1)
+	     else return (bak ++ show n)
+
+  bakname <- findBak 0
+  putStrLn$ "  [intel-aes] Backing up old .cabal file as " ++ show bakname
+  renameFile "intel-aes.cabal" bakname
+  curd <- getCurrentDirectory 
+  writeFile "intel-aes.cabal" (unlines$ map (hackLine curd libd) $ lines orig)
+  putStrLn$ "  [intel-aes] Rewrote .cabal file to set RPATH."
 
   -- Now call the default hook with a stripped down version:
   (postConf simpleUserHooks) args conf desc2 localinfo
@@ -46,9 +80,18 @@ stripLib lib = lib { libBuildInfo = stripBI (libBuildInfo lib) }
 stripExe exe = exe { buildInfo = stripBI (buildInfo exe) }
 stripBI bi   = bi  { extraLibs = filter filt (extraLibs bi) }
 
--- filt "intel_aes" = False
--- filt _           = True
 filt str = not (isInfixOf "intel_aes" str)
+
+-- DANGER DANGER:
+--   ld-options: -Wl,-rpath=/opt/intel-aes/cbits/
+hackLine curd libd line 
+  --    | isInfixOf "-Wl,-rpath=" line =  "ld-options: -Wl,-rpath=" ++ libd
+  --
+    -- The extra-lib-dirs is for the benefit of the build process:
+    | isInfixOf "extra-lib-dirs: ./cbits/" line =  "  extra-lib-dirs: " ++ curd ++"/cbits/"
+    | isInfixOf "ld-options: HACKME"       line =  "  ld-options: -Wl,-rpath=" ++ libd
+    | otherwise                                 =  line
+
 
 ------------------------------------------------------------
 my_preBuild :: Args -> BuildFlags -> IO HookedBuildInfo
@@ -63,22 +106,17 @@ my_preBuild args flags = do
   putStrLn$ "  [intel-aes] Done with external build job."
   putStrLn$ "================================================================================\n"
 
-  -- We add this here because we don't
-  -- FIXME: This should actually be the INSTALL PATH, but I haven't successfully extracted it.
-  let newflags = flags { buildProgramArgs = 
-			  ("ld",["-rpath="++ rootdir ++"/cbits/"]) : (buildProgramArgs flags)
-		       }
-
   (preBuild simpleUserHooks) args flags
 
 
-
+--------------------------------------------------------------------------------
 my_build :: PackageDescription -> LocalBuildInfo -> UserHooks -> BuildFlags -> IO ()
 my_build desc linfo hooks flags = do 
-  putStrLn$ "  [intel-aes] Modyfing the build action to include the .a file..."
+  putStrLn$ "  [intel-aes] Build action started."
+--  putStrLn$ "  [intel-aes] Modyfing the build action to include the .a file..."
   putStrLn$ " \nBUILD FLAGS: "++ show flags ++"\n"
 -- Doesn't work, get error "setup: internal error InstallDirs.libsubdir"
---  putStrLn$ "INstall dirs: " ++ show (absoluteInstallDirs desc linfo NoCopyDest)
+--  putStrLn$ "Install dirs: " ++ show (absoluteInstallDirs desc linfo NoCopyDest)
 -- absoluteInstallDirs :: PackageDescription -> LocalBuildInfo -> CopyDest -> InstallDirs FilePathSource
 
 -- Is there another way to get out the install dirs?
@@ -86,6 +124,29 @@ my_build desc linfo hooks flags = do
 -- installDirTemplates :: InstallDirTemplates
 --     The installation directories for the various differnt kinds of files 
 
+  -- rootdir <- getCurrentDirectory 
+  -- We add this here because we can't do it in the .cabal file and have it be variable:
+  -- FIXME: This should actually be the INSTALL PATH, but I haven't successfully extracted it.
+
+  -- let newflags = flags { buildProgramArgs = 
+  -- 			  ("ld",["-rpath="++ rootdir ++"/cbits/"]) : (buildProgramArgs flags)  }
 
   (buildHook simpleUserHooks) desc linfo hooks flags
   putStrLn$ "  [intel-aes] Build action finished.\n\n"
+
+
+
+--------------------------------------------------------------------------------
+my_install :: PackageDescription -> LocalBuildInfo -> UserHooks -> InstallFlags -> IO ()
+my_install desc linfo hooks flags = do 
+  putStrLn$ "  [intel-aes] Install action:"
+  putStrLn$ "\n================================================================================"
+  --libd <- readIORef global_var_hack 
+  libd <- readFile tmpfile 
+  let dest = (libd ++ "/libintel_aes.so")
+  putStrLn$ "  [intel-aes] Copying shared library to: " ++ show dest
+  copyFile "./cbits/libintel_aes.so" dest
+  putStrLn$ "  [intel-aes] Done copying."
+  -- removeFile tmpfile -- Might install more than once, right?
+  putStrLn$ "================================================================================\n"
+  (instHook simpleUserHooks) desc linfo hooks flags
